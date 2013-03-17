a42ada6055738fa2f0d00e027a0ef65cd7c5fdc4
Tom Leaman <tom@tomleaman.co.uk>
2013-03-17 18:25:30 +0000

    Add other params for track

--- checkpoint_manager/src/event/Track.java
+++ checkpoint_manager/src/event/Track.java
@@ -3,9 +3,15 @@ package event;
 public class Track {
 
   private char id;
+  private Node start;
+  private Node end;
+  private int safeTime;
   
-  public Track(char id) {
+  public Track(char id, Node start, Node end, int safeTime) {
     setId(id);
+    setStartNode(start);
+    setEndNode(end);
+    setSafeTime(safeTime);
   }
   
   public char getId() {
@@ -14,6 +20,30 @@ public class Track {
   
   private void setId(char id) {
     this.id = id;
+  }
+  
+  public Node getStartNode() {
+    return start;
+  }
+  
+  private void setStartNode(Node start) {
+    this.start = start;
+  }
+  
+  public Node getEndNode() {
+    return end;
+  }
+  
+  private void setEndNode(Node end) {
+    this.end = end;
+  }
+  
+  public int getSafeTime() {
+    return safeTime;
+  }
+  
+  private void setSafeTime(int safeTime) {
+    this.safeTime = safeTime;
   }
   
 }
