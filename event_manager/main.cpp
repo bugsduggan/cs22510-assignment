7b4f553264aad0d98fd94388ee2d83e60e9dff34
Tom Leaman <tom@tomleaman.co.uk>
2013-03-19 18:33:25 +0000

    Add single query

--- event_manager/main.cpp
+++ event_manager/main.cpp
@@ -6,12 +6,13 @@ int show_menu() {
 	int response;
 
 	cout << "Main menu:" << endl;
-	cout << "\t1. Count competitors waiting to start" << endl;
-	cout << "\t2. Count competitors running" << endl;
-	cout << "\t3. Count competitors finished" << endl;
-	cout << "\t4. Count competitors excluded" << endl;
-	cout << "\t5. List all competitors' status" << endl;
-	cout << "\t6. Quit" << endl;
+	cout << "\t1. Query individual competitor" << endl;
+	cout << "\t2. Count competitors waiting to start" << endl;
+	cout << "\t3. Count competitors running" << endl;
+	cout << "\t4. Count competitors finished" << endl;
+	cout << "\t5. Count competitors excluded" << endl;
+	cout << "\t6. List all competitors' status" << endl;
+	cout << "\t7. Quit" << endl;
 	cout << endl;
 
 	cout << ">> ";
@@ -24,13 +25,33 @@ int main(int argc, char* argv[]) {
 	using namespace std;
 
 	int input = -1;
+	bool running = true;
 
 	cout << "Event manager" << endl;
 	cout << endl;
 
-	while (input != 6) {
+	while (running) {
 		input = show_menu();
-		cout << input << endl;
+		switch (input) {
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			case 6:
+				break;
+			case 7:
+				running = false;
+				break;
+			default:
+				// invalid input, do nothing
+				break;
+		}
 	}
 
 	cout << "Program terminated" << endl;
