95f314f9679a77a0dd17d34fdf24f0cf2c20d212
Tom Leaman <tom@tomleaman.co.uk>
2013-03-21 22:57:03 +0000

    Add courses

--- event_creator/main.cpp
+++ event_creator/main.cpp
@@ -7,6 +7,7 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <vector>
 
 int show_menu() {
 	using namespace std;
@@ -103,6 +104,49 @@ void add_entrant() {
 	write_entrant(filename, id, course, name);
 }
 
+void write_course(std::string filename, char id, int num_nodes, std::vector<int> nodes) {
+	using namespace std;
+
+	ofstream file;
+	file.open(filename.c_str(), ios::app);
+
+	file << id << " " << num_nodes;
+	for (int i = 0; i < num_nodes; i++) {
+		file << " " << nodes[i];
+	}
+	file << "\n";
+
+	file.close();
+}
+
+void add_course() {
+	using namespace std;
+
+	cout << "Please enter course id" << endl;
+	cout << ">> ";
+	char id;
+	cin >> id;
+
+	cout << "Please enter the number of nodes" << endl;
+	cout << ">> ";
+	int num_nodes;
+	cin >> num_nodes;
+
+	vector<int> nodes(num_nodes);
+	for (int i = 0; i < num_nodes; i++) {
+		cout << "Please enter node " << (i+1) << endl;
+		cout << ">> ";
+		cin >> nodes[i];
+	}
+
+	cout << "Please enter course file" << endl;
+	cout << ">> ";
+	string filename;
+	cin >> filename;
+
+	write_course(filename, id, num_nodes, nodes);
+}
+
 int main(int argc, char* argv[]) {
 	using namespace std;
 
@@ -119,6 +163,7 @@ int main(int argc, char* argv[]) {
 				add_entrant();
 				break;
 			case 3:
+				add_course();
 				break;
 			case 4:
 				running = false;
