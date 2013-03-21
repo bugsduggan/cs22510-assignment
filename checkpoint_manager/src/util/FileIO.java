7366cad654f32d031ce6d485aaa695a8d0b4326b
Tom Leaman <tom@tomleaman.co.uk>
2013-03-21 20:22:53 +0000

    Add file locking to java

--- checkpoint_manager/src/util/FileIO.java
+++ checkpoint_manager/src/util/FileIO.java
@@ -1,13 +1,13 @@
 package util;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.nio.channels.FileLock;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -38,8 +38,16 @@ public class FileIO {
 	public static void appendToFile(String filename, String text) {
 		File f = new File(filename);
 		try {
-			PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(f)), true);
-			out.println(text);
+			FileOutputStream fos = new FileOutputStream(f, true);
+			FileLock fl = fos.getChannel().tryLock();
+			while (fl == null) {
+				fl = fos.getChannel().tryLock();
+			}
+			FileWriter out = new FileWriter(fos.getFD());
+			out.write(text + "\n");
+
+			fl.release();
+			fos.close();
 			out.close();
 		} catch (IOException e) {
 			e.printStackTrace();
